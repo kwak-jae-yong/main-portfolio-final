ba4e2532e3b28d0416f97ed21abc03f666be8c7c
kwak-jae-yong <dream001211@naver.com>
2025-05-29 15:38:23 +0900

    Add files via upload

---
+++ js/theme.js
@@ -0,0 +1,7 @@
+  document.addEventListener("DOMContentLoaded", function () {
+    const modeBtn = document.querySelector(".modeBtn");
+    modeBtn.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.body.classList.toggle("light-mode");
+    });
+  });
